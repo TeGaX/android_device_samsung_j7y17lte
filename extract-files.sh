508b55f74f3cd931b42b6dc2c5136452e9942a24
Tenshi2112 <63868370+TeGaX@users.noreply.github.com>
2020-11-20 10:39:07 +0700

    Update extract-files.sh

--- extract-files.sh
+++ extract-files.sh
@@ -24,9 +24,9 @@ DEVICE=j7y17lte
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-LINEAGE_ROOT="${MY_DIR}"/../../..
+ARROW_ROOT="${MY_DIR}"/../../..
 
-HELPER="${LINEAGE_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
+HELPER="${LINEAGE_ROOT}/vendor/arrow/build/tools/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
