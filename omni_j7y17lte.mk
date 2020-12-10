570745aeaf3399b1873baa820a55258b9acd3ac4
Tenshi2112 <63868370+TeGaX@users.noreply.github.com>
2020-12-10 21:59:24 +0700

    Update omni_j7y17lte.mk

--- omni_j7y17lte.mk
+++ omni_j7y17lte.mk
@@ -16,7 +16,6 @@
 
 PRODUCT_RELEASE_NAME := j7y17lte
 
-$(call inherit-product, build/target/product/embedded.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
